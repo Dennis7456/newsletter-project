166f67fad24dfd68241676daaa174460840c0335
Dennis7456 <denniskiplangat.dk@gmail.com>
2023-09-18 23:42:12 +0300

    added newsletter component and relevant styles

---
+++ src/Newsletter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Newsletter.css';
+
+function Newsletter(props) {
+    const [email, setEmail] = React.useState('');
+    const emailPartsCount = countEmailParts(email);
+
+  return (
+    <section className='es-newsletter'>
+        <div className='es-newsletter_spectrum' aria-hidden>
+            {Array.from(Array(5)).map((_, i) => (
+                <div className={`es-newsletter_bar ${i + 1 <= emailPartsCount && 'es-newsletter_bar--active'}`} key={i}></div>
+            ))}
+        </div>
+        <header className='es-newsletter_header'>
+            <h2>Get the newsletter</h2>
+        </header>
+        <input className='es-newsletter_email' type='email' placeholder='your email' value={email} onChange={(evt) => setEmail(evt.target.value)} />
+        <button className={`es-newsletter_submit`}>Sign up</button>    
+    </section>
+  )
+}
+
+export default Newsletter
+
+function countEmailParts(email) {
+    if (/@.+\..{2,}$/.test(email)) {
+        return 5;
+    } else if(/@.+\..?$/.test(email)) {
+        return 4;
+    } else if(/@.+$/.test(email)) {
+        return 3;
+    } else if (/@/.test(email)) {
+        return 2;
+    } else if (/.+/.test(email)) {
+        return 1;
+    } else {
+        return 0
+    }
+}
